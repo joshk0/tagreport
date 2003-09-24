417a7bc04ad66628d022b15e7836b35f17edb7ce
Joshua Kwan <joshk@triplehelix.org>
2003-09-24 03:34:53 +0000

    kill extraneous whitespace
    git-svn-id: file:///svn/tagreport/trunk@110 8f5d9d0c-67d6-0310-8973-bd7ee401cfa4

--- util.cc
+++ util.cc
@@ -17,16 +17,16 @@ using namespace std;
 /* Allocates stuff on the heap - free it! */
 char* get_time_string (void)
 {
-    struct tm *now;
-    time_t now_secs;
-    char now_date[18];
+  struct tm *now;
+  time_t now_secs;
+  char now_date[18];
 
-    /* Get the current time */
-    time(&now_secs);
-    now = localtime(&now_secs);
-    strftime (now_date, 18, "%H:%M, %Y-%m-%d", now);
+  /* Get the current time */
+  time(&now_secs);
+  now = localtime(&now_secs);
+  strftime (now_date, 18, "%H:%M, %Y-%m-%d", now);
 
-    return strdup(now_date);
+  return strdup(now_date);
 }
 
 /* Escapes illegal HTML characters into their long counterparts. */
